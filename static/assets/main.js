7d5f5878fa0cff0ebd0b141c895f0bae83928a70
César Del Solar <delsolar@gmail.com>
2023-03-04 15:50:38 -0500

    edit for being a subdomain

--- static/assets/main.js
+++
@@ -1,8 +0,0 @@
-const localDarkTheme = window.localStorage && window.localStorage.getItem("darkMode");
-document.body.classList.remove("light-theme", "dark-theme");
-
-if (localDarkTheme) {
-  document.body.classList.add(localDarkTheme === "true" ? "dark-theme" : "light-theme");
-} else {
-  document.body.classList.add("light-theme");
-}
