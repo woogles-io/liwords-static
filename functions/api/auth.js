4657971a9ad7c57623851d15026198e8887c30b2
César Del Solar <delsolar@gmail.com>
2023-03-04 20:20:02 -0500

    i dont understand wtf is going on

--- functions/api/auth.js
+++ functions/api/auth.js
@@ -15,7 +15,7 @@ export async function onRequest(context) {
         const redirectUrl = new URL('https://github.com/login/oauth/authorize');
         redirectUrl.searchParams.set('client_id', client_id);
         redirectUrl.searchParams.set('redirect_uri', url.origin + '/api/callback');
-        redirectUrl.searchParams.set('scope', 'user:email');
+        redirectUrl.searchParams.set('scope', 'public_repo user');
         redirectUrl.searchParams.set(
             'state',
             crypto.getRandomValues(new Uint8Array(12)).join(''),
